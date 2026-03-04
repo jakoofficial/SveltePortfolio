615cde044040fe4689e6f9c8b9af055818ba3123
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-03-04 12:06:53 +0100

    Here go ahead... Added footer and variables to css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to portfolio</h1>
+<section>
+	<h1>Welcome to portfolio</h1>
+</section>
+<footer>
+	<p>BoldByte &copy2026</p>
+</footer>
 
+<style>
+	h1 {
+		color: var(--title-color);
+	}
+
+    footer{
+        width: 100%;
+        height: 50px;
+        border-top: 1px solid #00000050;
+    }
+    footer p {
+        position: relative;
+    }
+</style>
